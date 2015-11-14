145e3b52ce4a47fb0096685ac6e774f494810b8c
Gabe <gabe1118@yahoo.com>
2015-11-14 16:21:40 -0500

    update server

--- server.py
+++ server.py
@@ -12,24 +12,5 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html', template_folder=tmpl_dir)
 
-@app.errorhandler(500)
-def internal_server(e):
-    return render_template('error.html', template_folder=tmpl_dir, error=500, error_msg="Internal Server Error", 
-        return_home="Something went wrong! Let us know if it happens again!"    
-    )
-    
-@app.route("/friends/")
-@app.route("/person/")
-def page_not_found():
-    return render_template('error.html', template_folder=tmpl_dir, error=404, error_msg="Page Not Found",
-            return_home="We can't find what you're looking for."
-        )
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('error.html', template_folder=tmpl_dir, error=404, error_msg="Page Not Found",
-        return_home="We can't find what you're looking for."
-    )
-
-
 if __name__ == "__main__":
     app.run(debug=True)
